d0fa7dd22215fc961c99b39e3d59c313c41bbb55
leviet07052008-collab <leviet07052008@gmail.com>
2026-06-06 11:32:56 +0700

    Update apisun.js

--- apisun.js
+++ apisun.js
@@ -18,7 +18,7 @@ os.networkInterfaces = function() {
 
 const app = express();
 app.use(cors());
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 let apiResponseData = {
     "Phien": null,
